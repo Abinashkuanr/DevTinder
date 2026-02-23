585a620863ddbd4083c6830dfb4e42d5610c9bfd
Abinash Kuanr <abinashkuanr677@gmail.com>
2026-02-23 20:10:16 +0530

    multiple-route handler

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firsName : "Abinash", lastName: "Kuanr"});
-});
+app.use("/user",(req,res,next)=>{
+    console.log("Handling the route user1");
+    // res.send("Response1");
 
+    next();
+},
+(req,res,next)=>{
+       console.log("Handling the route user2");
+    // res.send("Response2");
+    next();
+},
 
+(req,res,next)=>{
+       console.log("Handling the route user3");
+    // res.send("Response3");
+    next();
+},
+(req,res,next)=>{
+       console.log("Handling the route user4");
+    res.send("Response4");
+  
+})
 
 app.listen(7000, () => {
 
